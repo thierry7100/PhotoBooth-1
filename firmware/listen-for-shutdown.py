d9c484ad8d6d726849b1e7acddd2d4e063028fc7
Jerome <jerome@labidurie.fr>
2018-01-07 21:32:30 +0100

    Add photobooth start/stop function to power button

--- firmware/listen-for-shutdown.py
+++ firmware/listen-for-shutdown.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
 
-import RPi.GPIO as GPIO
-import subprocess
+from gpiozero import Button
+from subprocess import check_call, CalledProcessError
+from signal import pause
 
+shutdown_btn = Button(3, hold_time=2)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+def shutdown():
+	print ("btn held")
+	check_call(['poweroff'])
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+def stop_start():
+	print ("btn released")
+	try:
+		check_call(['systemctl', 'status', 'photobooth.service'])
+		# no error, service is running, stop it
+		print("Stopping photobooth")
+		check_call(['systemctl', 'stop', 'photobooth.service'])
+	except CalledProcessError:
+		# error, service is not running, start it
+		print("Starting photobooth")
+		check_call(['systemctl', 'start', 'photobooth.service'])
+
+# GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.wait_for_edge(3, GPIO.FALLING)
+
+shutdown_btn.when_held = shutdown
+shutdown_btn.when_released = stop_start
+
+pause()
