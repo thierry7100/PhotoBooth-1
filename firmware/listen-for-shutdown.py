65dd781908b264bd9d259426f1c1c1e500df1ed3
jerome-labidurie <jerome@labidurie.fr>
2018-01-07 21:41:28 +0100

    remove output

--- firmware/listen-for-shutdown.py
+++ firmware/listen-for-shutdown.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
+# press/release quickly button for start/stop photobooth service
+# hold button for >2s for shutdown
+
 
 from gpiozero import Button
 from subprocess import check_call, CalledProcessError
 from signal import pause
+import os
 
 shutdown_btn = Button(3, hold_time=2)
+FNULL = open(os.devnull, 'w')
 
 def shutdown():
-	print ("btn held")
+	#print ("btn held")
 	check_call(['poweroff'])
 
 def stop_start():
-	print ("btn released")
+	#print ("btn released")
 	try:
-		check_call(['systemctl', 'status', 'photobooth.service'])
+		check_call(['systemctl', 'status', 'photobooth.service'], stdout=FNULL, stderr=subprocess.STDOUT)
 		# no error, service is running, stop it
-		print("Stopping photobooth")
+		#print("Stopping photobooth")
 		check_call(['systemctl', 'stop', 'photobooth.service'])
 	except CalledProcessError:
 		# error, service is not running, start it
-		print("Starting photobooth")
+		#print("Starting photobooth")
 		check_call(['systemctl', 'start', 'photobooth.service'])
 
 # GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
